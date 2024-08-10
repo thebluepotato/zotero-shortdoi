fc5a38393ea33912eee1fda066640ee2446677ac
Jonas Zaugg <jonas.zaugg@yahoo.com>
2024-08-11 01:07:13 +0200

    Update release script

---
+++ release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read -p 'Enter version number: ' version
+rm -f zotero-doi-manager-"$version".xpi
+zip -r zotero-doi-manager-"$version".xpi content/* locale/* skin/* bootstrap.js chrome.manifest install.rdf manifest.json prefs.js zoteroshortdoi.js
